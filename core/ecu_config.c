e5689e126f1cc523fe0e229697297c932ae72e50
Jakub Michalik <michalik.jakub@gmail.com>
2025-02-08 10:20:00 +0100

    config defaults implementation

---
+++ core/ecu_config.c
@@ -0,0 +1,26 @@
+#include "core/ecu_config.h"
+
+void ecu_config_defaults(ecu_config_t *cfg)
+{
+    /* RPM */
+    cfg->rpm_idle = 45000.0f;
+    cfg->rpm_max = 120000.0f;
+    cfg->rpm_start_target = 60000.0f;
+
+    /* EGT */
+    cfg->egt_max = 1100.0f;
+    cfg->egt_start_min = 150.0f;
+
+    /* Fuel */
+    cfg->fuel_start_pct = 8.0f;
+    cfg->fuel_max_pct = 100.0f;
+    cfg->fuel_ramp_rate = 5.0f;
+
+    /* Timeouts */
+    cfg->prestart_timeout_ms = 5000;
+    cfg->spinup_timeout_ms = 10000;
+    cfg->ignition_timeout_ms = 8000;
+
+    /* Ramp */
+    cfg->rpm_ramp_rate = 400.0f;
+}
