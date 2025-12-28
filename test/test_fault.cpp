b5bf0b47706e967d9fc9865b1ce084b1dddfbc45
Jakub Michalik <michalik.jakub@gmail.com>
2025-12-28 13:30:00 +0100

    test: convert map + fault tests to gtest

--- test/test_fault.cpp
+++ test/test_fault.cpp
@@ -1,98 +1,78 @@
-#include "test/test_runner.h"
-#include "core/ecu_fault.h"
+#include <gtest/gtest.h>
 
-TEST(test_fault_report)
-{
+extern "C" {
+#include "core/ecu_fault.h"
+}
+
+TEST(Fault, Report) {
     ecu_fault_mgr_t mgr;
     ecu_fault_init(&mgr);
 
     ecu_fault_report(&mgr, 0x01, FAULT_SEV_WARNING, FAULT_ACTION_NONE, 100);
-    ASSERT_EQ(ecu_fault_count(&mgr), 1);
-    ASSERT(ecu_fault_is_active(&mgr, 0x01));
+    EXPECT_EQ(ecu_fault_count(&mgr), 1);
+    EXPECT_TRUE(ecu_fault_is_active(&mgr, 0x01));
 }
 
-TEST(test_fault_clear)
-{
+TEST(Fault, Clear) {
     ecu_fault_mgr_t mgr;
     ecu_fault_init(&mgr);
 
     ecu_fault_report(&mgr, 0x01, FAULT_SEV_WARNING, FAULT_ACTION_NONE, 100);
     ecu_fault_clear(&mgr, 0x01);
-    ASSERT_EQ(ecu_fault_count(&mgr), 0);
-    ASSERT(!ecu_fault_is_active(&mgr, 0x01));
+    EXPECT_EQ(ecu_fault_count(&mgr), 0);
+    EXPECT_FALSE(ecu_fault_is_active(&mgr, 0x01));
 }
 
-TEST(test_fault_latched)
-{
+TEST(Fault, Latched) {
     ecu_fault_mgr_t mgr;
     ecu_fault_init(&mgr);
 
-    /* Critical faults are latched */
     ecu_fault_report(&mgr, 0x10, FAULT_SEV_CRITICAL, FAULT_ACTION_SHUTDOWN, 100);
-    ecu_fault_clear(&mgr, 0x10);  /* should not clear */
-    ASSERT(ecu_fault_is_active(&mgr, 0x10));
+    ecu_fault_clear(&mgr, 0x10);
+    EXPECT_TRUE(ecu_fault_is_active(&mgr, 0x10));
 }
 
-TEST(test_fault_priority)
-{
+TEST(Fault, Priority) {
     ecu_fault_mgr_t mgr;
     ecu_fault_init(&mgr);
 
     ecu_fault_report(&mgr, 0x01, FAULT_SEV_WARNING, FAULT_ACTION_DEGRADE, 100);
-    ASSERT_EQ(ecu_fault_get_action(&mgr), FAULT_ACTION_DEGRADE);
+    EXPECT_EQ(ecu_fault_get_action(&mgr), FAULT_ACTION_DEGRADE);
 
     ecu_fault_report(&mgr, 0x02, FAULT_SEV_CRITICAL, FAULT_ACTION_SHUTDOWN, 200);
-    ASSERT_EQ(ecu_fault_get_action(&mgr), FAULT_ACTION_SHUTDOWN);
+    EXPECT_EQ(ecu_fault_get_action(&mgr), FAULT_ACTION_SHUTDOWN);
 }
 
-TEST(test_fault_retry)
-{
+TEST(Fault, Retry) {
     ecu_fault_mgr_t mgr;
     ecu_fault_init(&mgr);
 
     ecu_fault_report(&mgr, 0x05, FAULT_SEV_WARNING, FAULT_ACTION_DEGRADE, 100);
     ecu_fault_set_retries(&mgr, 0x05, 3);
 
-    ASSERT(ecu_fault_try_retry(&mgr, 0x05));
-    ASSERT(!ecu_fault_is_active(&mgr, 0x05));
+    EXPECT_TRUE(ecu_fault_try_retry(&mgr, 0x05));
+    EXPECT_FALSE(ecu_fault_is_active(&mgr, 0x05));
 
-    /* Report again, retry again */
     ecu_fault_report(&mgr, 0x05, FAULT_SEV_WARNING, FAULT_ACTION_DEGRADE, 200);
     ecu_fault_set_retries(&mgr, 0x05, 3);
 }
 
-TEST(test_fault_log)
-{
+TEST(Fault, Log) {
     ecu_fault_mgr_t mgr;
     ecu_fault_init(&mgr);
 
     ecu_fault_report(&mgr, 0x01, FAULT_SEV_WARNING, FAULT_ACTION_NONE, 100);
     ecu_fault_report(&mgr, 0x02, FAULT_SEV_CRITICAL, FAULT_ACTION_SHUTDOWN, 200);
-    ASSERT(mgr.log_count >= 2);
+    EXPECT_GE(mgr.log_count, 2);
 }
 
-TEST(test_fault_degrade_mode)
-{
+TEST(Fault, DegradeMode) {
     ecu_fault_mgr_t mgr;
     ecu_fault_init(&mgr);
 
     ecu_fault_report(&mgr, 0x01, FAULT_SEV_WARNING, FAULT_ACTION_DEGRADE, 100);
-    ASSERT_EQ(ecu_fault_get_action(&mgr), FAULT_ACTION_DEGRADE);
+    EXPECT_EQ(ecu_fault_get_action(&mgr), FAULT_ACTION_DEGRADE);
 
-    /* Clear it */
     ecu_fault_clear(&mgr, 0x01);
-    ASSERT_EQ(ecu_fault_get_action(&mgr), FAULT_ACTION_NONE);
-}
-
-int main(void)
-{
-    printf("=== Fault Tests ===\n");
-    RUN_TEST(test_fault_report);
-    RUN_TEST(test_fault_clear);
-    RUN_TEST(test_fault_latched);
-    RUN_TEST(test_fault_priority);
-    RUN_TEST(test_fault_retry);
-    RUN_TEST(test_fault_log);
-    RUN_TEST(test_fault_degrade_mode);
-    TEST_SUMMARY();
+    EXPECT_EQ(ecu_fault_get_action(&mgr), FAULT_ACTION_NONE);
 }
---
+++ test/test_map.cpp
@@ -0,0 +1,62 @@
+#include <gtest/gtest.h>
+
+extern "C" {
+#include "core/ecu_map.h"
+}
+
+TEST(Map1D, Basic) {
+    ecu_map1d_t map = {};
+    map.x[0] = 0; map.x[1] = 50; map.x[2] = 100;
+    map.y[0] = 0; map.y[1] = 25; map.y[2] = 100;
+    map.count = 3;
+
+    EXPECT_NEAR(ecu_map1d_lookup(&map, 0), 0.0f, 0.1f);
+    EXPECT_NEAR(ecu_map1d_lookup(&map, 50), 25.0f, 0.1f);
+    EXPECT_NEAR(ecu_map1d_lookup(&map, 100), 100.0f, 0.1f);
+}
+
+TEST(Map1D, Interpolation) {
+    ecu_map1d_t map = {};
+    map.x[0] = 0; map.x[1] = 100;
+    map.y[0] = 0; map.y[1] = 200;
+    map.count = 2;
+
+    EXPECT_NEAR(ecu_map1d_lookup(&map, 50), 100.0f, 0.1f);
+    EXPECT_NEAR(ecu_map1d_lookup(&map, 25), 50.0f, 0.1f);
+}
+
+TEST(Map1D, Clamp) {
+    ecu_map1d_t map = {};
+    map.x[0] = 10; map.x[1] = 90;
+    map.y[0] = 5;  map.y[1] = 45;
+    map.count = 2;
+
+    EXPECT_NEAR(ecu_map1d_lookup(&map, -100), 5.0f, 0.1f);
+    EXPECT_NEAR(ecu_map1d_lookup(&map, 200), 45.0f, 0.1f);
+}
+
+TEST(Map2D, Basic) {
+    ecu_map2d_t map = {};
+    map.row_keys[0] = 0; map.row_keys[1] = 100;
+    map.col_keys[0] = 0; map.col_keys[1] = 100;
+    map.values[0][0] = 0;  map.values[0][1] = 10;
+    map.values[1][0] = 20; map.values[1][1] = 30;
+    map.rows = 2; map.cols = 2;
+
+    EXPECT_NEAR(ecu_map2d_lookup(&map, 0, 0), 0.0f, 0.1f);
+    EXPECT_NEAR(ecu_map2d_lookup(&map, 0, 100), 10.0f, 0.1f);
+    EXPECT_NEAR(ecu_map2d_lookup(&map, 100, 0), 20.0f, 0.1f);
+    EXPECT_NEAR(ecu_map2d_lookup(&map, 100, 100), 30.0f, 0.1f);
+}
+
+TEST(Map2D, Bilinear) {
+    ecu_map2d_t map = {};
+    map.row_keys[0] = 0; map.row_keys[1] = 100;
+    map.col_keys[0] = 0; map.col_keys[1] = 100;
+    map.values[0][0] = 0;  map.values[0][1] = 10;
+    map.values[1][0] = 20; map.values[1][1] = 30;
+    map.rows = 2; map.cols = 2;
+
+    float center = ecu_map2d_lookup(&map, 50, 50);
+    EXPECT_NEAR(center, 15.0f, 0.5f);
+}
